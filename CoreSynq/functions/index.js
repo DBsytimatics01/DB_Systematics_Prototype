84dc5ff015f3d42d0ed643f95d2b9deac0174243
DBsytimatics01 <zanderartist22@gmail.com>
2025-10-29 19:31:00 -0600

    Update index.js

--- CoreSynq/functions/index.js
+++ CoreSynq/functions/index.js
@@ -1,1 +1,11 @@
+// functions/index.js
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
+// simple HTTP function for health
+exports.hello = functions.https.onRequest((req,res)=>{
+  res.status(200).send("CoreSynq functions healthy");
+});
+
+// placeholder: create checkout session route will go here later (server side)
