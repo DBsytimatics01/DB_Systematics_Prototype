bf4b8a634acb797bc0fc642c4934eee424a43daf
DBsytimatics01 <zanderartist22@gmail.com>
2025-10-29 19:48:08 -0600

    Update ToolPalette.jsx

--- CoreSynq/src/ components/ToolPalette.jsx
+++ CoreSynq/src/ components/ToolPalette.jsx
@@ -1,1 +1,16 @@
+import React from "react";
 
+export default function ToolPalette({ tools, onSelect }) {
+  return (
+    <div className="tool-palette">
+      <h2>Tool Palette</h2>
+      <div className="tool-grid">
+        {tools.map((tool) => (
+          <button key={tool.id} onClick={() => onSelect(tool)}>
+            {tool.name}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+}
