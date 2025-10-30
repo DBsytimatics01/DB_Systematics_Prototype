b2fbce4575c03f4737913eb16eae69d7157aeb11
DBsytimatics01 <zanderartist22@gmail.com>
2025-10-30 07:59:32 -0600

    Update Navbar.jsx

--- CoreSynq/src/components/Navbar.jsx
+++ CoreSynq/src/components/Navbar.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
 
+export default function Navbar() {
+  return (
+    <nav className="flex justify-between px-6 py-4 bg-gray-800 shadow-md">
+      <h1 className="font-bold text-xl text-blue-400">CoreSynq</h1>
+      <div className="flex gap-4">
+        <Link to="/">Home</Link>
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/demo">Demo</Link>
+        <Link to="/sop">SOP</Link>
+        <Link to="/wallet">Wallet</Link>
+      </div>
+    </nav>
+  );
+}
