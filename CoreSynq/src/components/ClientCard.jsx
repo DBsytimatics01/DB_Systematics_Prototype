62fb605739fbe6470a686dad734519c5cfa8cd0c
DBsytimatics01 <zanderartist22@gmail.com>
2025-10-30 08:02:05 -0600

    Update ClientCard.jsx

--- CoreSynq/src/components/ClientCard.jsx
+++ CoreSynq/src/components/ClientCard.jsx
@@ -1,1 +1,10 @@
+import React from "react";
 
+export default function ClientCard({ name, profit }) {
+  return (
+    <div className="bg-gray-800 p-4 rounded-xl shadow-lg">
+      <h3 className="text-lg font-semibold">{name}</h3>
+      <p className="text-green-400 mt-2">Profit: {profit}</p>
+    </div>
+  );
+}
