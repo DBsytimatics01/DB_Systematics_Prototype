773fafecbece948a3cea1867c19acd3a501eb7dc
DBsytimatics01 <zanderartist22@gmail.com>
2025-10-29 19:51:49 -0600

    Update SOPPage.jsx

--- CoreSynq/src/ pages/SOPPage.jsx
+++ CoreSynq/src/ pages/SOPPage.jsx
@@ -1,1 +1,10 @@
+import React from "react";
 
+export default function SOPPage() {
+  return (
+    <div className="sop-page">
+      <h1>Standard Operating Procedures</h1>
+      <p>Define and manage your team workflows here.</p>
+    </div>
+  );
+}
