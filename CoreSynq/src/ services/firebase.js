2694d03acfc5aaff771587d97c6555c4b246b5c0
DBsytimatics01 <zanderartist22@gmail.com>
2025-10-29 19:28:47 -0600

    Update firebase.js

--- CoreSynq/src/ services/firebase.js
+++ CoreSynq/src/ services/firebase.js
@@ -1,1 +1,19 @@
+// src/services/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
+const firebaseConfig = {
+  // paste your firebase config here
+  apiKey: "REPLACE_ME",
+  authDomain: "REPLACE_ME",
+  projectId: "REPLACE_ME",
+  storageBucket: "REPLACE_ME",
+  messagingSenderId: "REPLACE_ME",
+  appId: "REPLACE_ME"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
